76c10ed6530c7e251cda5a44883c87fd84ded9cc
Your Name <you@example.com>
2018-04-09 16:48:52 -0500

    BackUp

--- BMNMovies/app/src/main/java/com/example/dev/bmnmovies/AdapterFilmes.java
+++ BMNMovies/app/src/main/java/com/example/dev/bmnmovies/AdapterFilmes.java
@@ -1,13 +1,15 @@
 package com.example.dev.bmnmovies;
 
 import android.content.Context;
-import android.media.Image;
+//import android.support.v7.app.AlertController;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -15,7 +17,7 @@ import java.util.ArrayList;
  * Created by aluno on 02/04/18.
  */
 
-public class AdapterFilmes extends BaseAdapter {
+public class AdapterFilmes extends RecyclerView.Adapter<AdapterFilmes.ViewHolder>{
 
     Context context;
     ArrayList<Filme> filmes;
@@ -26,39 +28,68 @@ public class AdapterFilmes extends BaseAdapter {
     }
 
     @Override
-    public int getCount() {
+    public AdapterFilmes.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View cell = null;
+        if (parent != null) {
+            this.context = parent.getContext();
+            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+            cell = inflater.inflate(R.layout.item_filme, parent, false);
+        }
+        return new ViewHolder(cell);
+
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+        if (holder != null){
+
+            holder.textFilme.setText( filmes.get(position).titulo);
+            Picasso.get().load(filmes.get(position).poster).into(holder.imageFilme);
+/*
+            holder.imageButtonSpeech.setOnClickListener {
+
+                var text = holder.textViewTitle.text.toString()
+                textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null)
+            }
+
+            holder.cardContainer.setOnClickListener {
+                if (side == 0){
+                    holder.textViewTitle.text = data[position].word
+                    holder.cardContainer.setBackgroundColor(context.resources.getColor(R.color.colorFrontSide))
+                    side = 1
+                }else{
+                    holder.textViewTitle.text = data[position].definition
+                    holder.cardContainer.setBackgroundColor(context.resources.getColor(R.color.colorBackSide))
+                    side = 0
+                }
+            }
+
+            holder.cardContainer.setOnLongClickListener {
+                listener.onItemClick(data[position])
+                true
+            }*/
+        }
+    }
+
+    @Override
+    public int getItemCount() {
         return filmes.size();
     }
 
-    @Override
-    public Object getItem(int i) {
-        return filmes.get(i);
-    }
 
-    @Override
-    public long getItemId(int i) {
-        return 0;
-    }
+    public class ViewHolder extends RecyclerView.ViewHolder {
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+       TextView textFilme ;
+       ImageView imageFilme;
+       public ViewHolder(View itemView) {
+           super(itemView);
 
-        if (convertView == null) {
-            convertView = LayoutInflater.from(context).
-                    inflate(R.layout.item_filme , parent, false);
-        }
+           if (itemView != null){
+               this.textFilme = itemView.findViewById(R.id.textFilme);
+               this.imageFilme = itemView.findViewById(R.id.imageFilme);
+           }
 
-        Filme item = (Filme) getItem(position);
-
-        TextView textFilme = convertView.findViewById(R.id.textFilme);
-        ImageView imageFilme = convertView.findViewById(R.id.imageFilme);
-
-        textFilme.setText(item.titulo);
-        //imageFilme.setText(item.titulo);
-
-        // returns the view for the current row
-        return convertView;
-
-
-    }
+       }
+   }
 }
--- BMNMovies/app/src/main/java/com/example/dev/bmnmovies/MainActivity.java
+++ BMNMovies/app/src/main/java/com/example/dev/bmnmovies/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.dev.bmnmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.Spinner;
 
@@ -12,8 +15,9 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private Spinner spinnerFilmes;
-    private ListView listview;
+    private RecyclerView listview;
     Spinner spinner;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +26,9 @@ public class MainActivity extends AppCompatActivity {
 
         spinner = (Spinner) findViewById(R.id.spinnerFilmes);
         // Create an ArrayAdapter using the string array and a default spinner layout
-        listview = (ListView) findViewById(R.id.listview);
+        listview = (RecyclerView) findViewById(R.id.listview);
+
+
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.Filmes_array, android.R.layout.simple_spinner_item);
@@ -37,9 +43,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
 
-                AdapterFilmes adapterFilmes = new AdapterFilmes(MainActivity.this,filmesTeste(spinner.getSelectedItem().toString()));
-                listview.setAdapter(adapterFilmes);
+                listview.setLayoutManager(new GridLayoutManager(MainActivity.this, 2));
 
+                AdapterFilmes adapterFilmes = new AdapterFilmes(MainActivity.this,filmesTeste(spinner.getSelectedItem().toString()));
+               // listview.setAdapter(AdapterFilmes);
+
+
+                listview.setAdapter(adapterFilmes);
+               // mRecyclerCardAdapter.notifyDataSetChanged()
 
             }
 
@@ -58,27 +69,31 @@ public class MainActivity extends AppCompatActivity {
     public ArrayList<Filme> filmesTeste(String genero){
         ArrayList<Filme> filmes = new ArrayList<Filme>();
         Filme f;
-
+        implementation 'com.squareup.picasso:picasso:2.71828'
         if (genero.equals("Ação")) {
 
             f = new Filme();
             f.titulo = "Mumia";
             f.genero = "Ação";
+            f.poster = "http://i.imgur.com/DvpvklR.png";
             filmes.add(f);
 
             f = new Filme();
             f.titulo = "Batman";
             f.genero = "Ação";
+            f.poster = "https://images-na.ssl-images-amazon.com/images/M/MV5BMTg2MzI1MTg3OF5BMl5BanBnXkFtZTgwNTU3NDA2MTI@._V1_SX300.jpg";
             filmes.add(f);
 
             f = new Filme();
             f.titulo = "Spiderman";
             f.genero = "Ação";
+            f.poster = "https://images-na.ssl-images-amazon.com/images/M/MV5BMTg2MzI1MTg3OF5BMl5BanBnXkFtZTgwNTU3NDA2MTI@._V1_SX300.jpg";
             filmes.add(f);
         }else{
             f = new Filme();
             f.titulo = "Mulan";
             f.genero = "Alimação";
+            f.poster = "https://images-na.ssl-images-amazon.com/images/M/MV5BMTg2MzI1MTg3OF5BMl5BanBnXkFtZTgwNTU3NDA2MTI@._V1_SX300.jpg";
             filmes.add(f);
 
         }
