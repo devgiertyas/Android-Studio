836278e5827ddab4fbdb79bc861f6a6819cb2621
Your Name <you@example.com>
2018-05-23 21:55:36 -0500

    Final

--- BMNMovies/app/src/main/java/com/example/dev/bmnmovies/AppControle.java
+++ BMNMovies/app/src/main/java/com/example/dev/bmnmovies/AppControle.java
@@ -79,20 +79,27 @@ public class AppControle {
 
 
     public String cadastrar(Context contexto, String nome,String login ,String senha,String resenha)
-
     {
+        if(nome.length() > 20 || nome.length() <= 3){
+            return "O nome deve conter no mínimo 6, e no máximo 20 caracteres";
+        }
+
+        if(login.length() > 20 || login.length() <= 4){
+            return "O login deve conter no mínimo 5, e no máximo 20 caracteres";
+        }
+
+        if(senha.length() <= 5 || senha.length() > 15){
+            return "Insira uma senha com no mínimo 6 e no máximo 15 caracteres";
+        }
 
         if(!senha.equals(resenha)){
-            return "Confirmação de senha incorreta";
-
+            return "Confirmação de senha não confere";
         }
-            //salvar no banco
 
+        //salvar no banco
         salvarNoDB(contexto,nome,login,senha);
 
-
         return "ok";
-
     }
 
     private void salvarNoDB(Context contexto, String nome,String login,String senha) {
